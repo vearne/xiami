ef3177d7f6331eee1c0144866bce466d84808cdb
vearne <vearne@github.com>
2016-02-05 17:52:41 +0800

    add parse_song.py

---
+++ parse_song.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+from lxml import etree
+from StringIO import StringIO
+import requests
+import random
+import os
+
+
+
+
+
+def main():
+
+if __name__ == '__main__':
+    main()
+
+
+
+
